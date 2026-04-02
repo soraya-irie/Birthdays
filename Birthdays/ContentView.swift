6d91de71e22072c01cc37bc2ccefe3335bb8eccf
soraya-ire <rujiangkongshi@gmail.com>
2026-04-03 07:29:14 +0900

    Complete Section3 Step7

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -36,6 +36,7 @@ struct ContentView: View {
                     }
                     Button("Save") {
                         let newFriend = Friend(name: newName, birthday: newDate)
+                        friends.append(newFriend)
                     }
                     .bold()
                 }
