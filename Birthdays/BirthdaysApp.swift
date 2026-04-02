8f893df1be446ba20c4df68da23e68f48c5ac957
soraya-ire <rujiangkongshi@gmail.com>
2026-04-02 17:59:23 +0900

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by soraya irie on 2026/04/02.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
