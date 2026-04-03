e763375c9fb06fd8dd4abce16281de26e61fd3ea
soraya-ire <rujiangkongshi@gmail.com>
2026-04-03 15:04:28 +0900

    Complete Section6 Step2

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Query private var friends: [Friend]
+    @Query(sort: \Friend.birthday) private var friends: [Friend]
     @Environment(\.modelContext) private var context
 
     @State private var newName = ""
